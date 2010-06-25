87e0aafbba8b0c277e0c6c1e24f35a8d13c3c88b
Marc Matteo <marcm@lectroid.net>
2010-06-24 21:57:02 -0700

    Use the modified property to generate 304 hearders.  Also return the actual datetime object.  More useful that way.

--- appengine-experimental/src/utils/conditional_http.py
+++ appengine-experimental/src/utils/conditional_http.py
@@ -1,7 +1,7 @@
-from google.appengine.ext import db
+import hashlib
+
 from models import CHPIncident
 
-import hashlib
 
 def getLastMod(webapp):
 	center = webapp.request.get("center")
@@ -9,7 +9,7 @@ def getLastMod(webapp):
 	area = webapp.request.get("area")
 
 	last_mod_query = CHPIncident.all()
-	last_mod_query.order('-updated')
+	last_mod_query.order('-modified')
 	if center != "":
 		last_mod_query.filter('CenterID =', center)
 	if dispatch != "":
@@ -19,13 +19,15 @@ def getLastMod(webapp):
 
 	last_mod_entity = last_mod_query.get()
 	if last_mod_entity is not None:
-		return last_mod_entity.updated.strftime("%a, %d %b %Y %H:%M:%S GMT")
+		return last_mod_entity.modified
 
 	return None
 
 
-def isNotModified(webapp, last_mod):
-	if last_mod is not None:
+def isNotModified(webapp, last_mod_date):
+	if last_mod_date is not None:
+		last_mod = last_mod_date.strftime("%a, %d %b %Y %H:%M:%S GMT")
+
 		# HTTP/1.1
 		if 'If-None-Match' in webapp.request.headers and webapp.request.headers['If-None-Match'] == hashlib.sha1(last_mod).hexdigest():
 			webapp.response.set_status(304)
@@ -39,7 +41,9 @@ def isNotModified(webapp, last_mod):
 	return False;
 
 
-def setConditionalHeaders(webapp, last_mod):
-	if last_mod is not None:
+def setConditionalHeaders(webapp, last_mod_date):
+	if last_mod_date is not None:
+		last_mod = last_mod_date.strftime("%a, %d %b %Y %H:%M:%S GMT")
+
 		webapp.response.headers["Last-Modified"] = last_mod
 		webapp.response.headers["ETag"] = '"'+hashlib.sha1(last_mod).hexdigest()+'"'
