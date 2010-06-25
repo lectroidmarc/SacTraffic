4d8903b6a1060948f11992daf9a588e15239a27f
Marc Matteo <marcm@lectroid.net>
2010-06-24 20:17:22 -0700

    More... almost there.

--- appengine-experimental/src/handlers/atom.py
+++ appengine-experimental/src/handlers/atom.py
@@ -34,7 +34,12 @@ class AtomHandler(webapp.RequestHandler):
 		})
 
 		ElementTree.SubElement(feed, 'title').text = 'Traffic'
+		ElementTree.SubElement(feed, 'subtitle').text = 'Traffic incidents from the CHP'
+		ElementTree.SubElement(ElementTree.SubElement(feed, 'author'), 'name').text = 'The California Highway Patrol'
+		ElementTree.SubElement(feed, 'id').text = 'tag:traffic.lectroid.net,2010-06-24:100'
+		ElementTree.SubElement(feed, 'updated').text = last_mod		# crap
 
+		# self link...
 		self_href = "http://traffic.lectroid.net/rss"
 		query_string = "?" + self.request.environ['QUERY_STRING']
 		if query_string != "?":
@@ -45,6 +50,8 @@ class AtomHandler(webapp.RequestHandler):
 			'rel': 'self',
 			'type': 'application/atom+xml'
 		})
+
+		# pubsubhubbub link...
 		ElementTree.SubElement(feed, 'link', {
 			'href': 'http://pubsubhubbub.appspot.com',
 			'rel': 'hub'
@@ -60,11 +67,10 @@ class AtomHandler(webapp.RequestHandler):
 			entry = ElementTree.SubElement (feed, 'entry')
 
 			ElementTree.SubElement(entry, 'title').text = incident.LogType
-			ElementTree.SubElement(entry, 'description').text = description
-			ElementTree.SubElement(entry, 'pubDate').text = incident.LogTime.strftime("%a, %d %b %Y %H:%M:%S GMT")
-			ElementTree.SubElement(entry, 'guid', {'isPermaLink': 'false'}).text = incident.LogID
-			ElementTree.SubElement(entry, 'source', {'url': 'http://media.chp.ca.gov/sa_xml/sa.xml'}).text = "CHP"
-			ElementTree.SubElement(entry, 'category').text = incident.LogTypeID
+			ElementTree.SubElement(entry, 'id').text = 'tag:traffic.lectroid.net,2010-06-24:' + incident.LogID
+			ElementTree.SubElement(entry, 'content', {'type': 'html'}).text = description
+			ElementTree.SubElement(entry, 'published').text = incident.LogTime.strftime("%Y-%m-%dT%H:%M:%SZ")
+			ElementTree.SubElement(entry, 'updated').text = incident.LogTime.strftime("%Y-%m-%dT%H:%M:%SZ")
 
 			if incident.geolocation is not None:
 				ElementTree.SubElement(entry, 'georss:point').text = str(incident.geolocation.lat) + " " + str(incident.geolocation.lon)
