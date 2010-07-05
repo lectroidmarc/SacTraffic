aaf051bf698fea3fb8091b7209f2492db92c7718
Marc Matteo <marcm@lectroid.net>
2010-07-04 21:48:32 -0700

    Split off the actual processing of the CHP data into its own function and add a POST handler as well.  That way we can feed this via an external script if we want (and we want).

--- appengine-experimental/src/handlers/update.py
+++ appengine-experimental/src/handlers/update.py
@@ -1,3 +1,5 @@
+import hashlib
+import hmac
 import logging
 import pickle
 import re
@@ -28,10 +30,8 @@ class UpdateHandler(webapp.RequestHandler):
 			touchActiveIncidents()
 		else:
 			if result.status_code == 200:
-				incident_list = []
-
 				try:
-					chpState = ElementTree.XML(result.content)
+					chp_etree = ElementTree.XML(result.content)
 				except ElementTree.ExpatError, e:
 					error = "XML processing error. %s" % e.message
 					logging.warning(error)
@@ -39,99 +39,119 @@ class UpdateHandler(webapp.RequestHandler):
 
 					touchActiveIncidents()
 				else:
-					for chpCenter in chpState:
-						for chpDispatch in chpCenter:
-							# For some reason, sometimes the Dispatch ID is blank
-							# so skip these.
-							if chpDispatch.attrib['ID'] == "":
-								continue
-
-							for chpLog in chpDispatch:
-								incident = CHPIncident.get_by_key_name(chpLog.attrib['ID'])
-								if incident is None:
-									incident = CHPIncident(key_name = chpLog.attrib['ID'],
-										CenterID = chpCenter.attrib['ID'],
-										DispatchID = chpDispatch.attrib['ID'],
-										LogID = chpLog.attrib['ID'],
-										LogTime = datetime.strptime(chpLog.find('LogTime').text, '"%m/%d/%Y %I:%M:%S %p"').replace(tzinfo=Pacific()),
-										Location = deCopIfy(chpLog.find('Location').text.strip('"')),
-										Area = chpLog.find('Area').text.strip('"'),
-										ThomasBrothers = chpLog.find('ThomasBrothers').text.strip('"'),
-										TBXY = chpLog.find('TBXY').text.strip('"'),
-										modified = datetime.utcnow()
-										)
-
-									#
-									# Geocoding
-									#
-									if incident.TBXY != "":
-										tbxy = incident.TBXY.partition(":")
-										if incident.CenterID == "STCC":
-											incident.geolocation = db.GeoPt(
-												lat = float(tbxy[2]) * 0.00000274 +	 33.172,
-												lon = float(tbxy[0]) * 0.0000035  - 144.966
-											)
-										elif incident.CenterID == "SLCC":
-											incident.geolocation = db.GeoPt(
-												lat = float(tbxy[2]) * 0.00000275 +	 30.054,
-												lon = float(tbxy[0]) * 0.00000329 - 126.589
-											)
-										elif incident.CenterID == "FRCC":
-											incident.geolocation = db.GeoPt(
-												lat = float(tbxy[2]) * 0.00000274 +	 30.84,
-												lon = float(tbxy[0]) * 0.00000335 -  141
-											)
-
-								#
-								# LogType/LogTypeID
-								#
-								# I *think* the LogType/LogTypeID can change as
-								# the situation evolves.
-								#
-								# (ex: 'Traffic Collision - No Details' could
-								# become 'Possible Fatality')
-								#
-								logtype = chpLog.find('LogType').text.strip('"').partition(" - ")
-								if incident.LogTypeID != logtype[0]:
-									incident.LogTypeID = logtype[0]
-									incident.LogType = logtype[2]
-									incident.modified = datetime.utcnow()
-
-
-								#
-								# Special handling for the LogDetails
-								#
-								LogDetails = {
-									'details': [],
-									'units': []
-								}
-								logdetails_element = chpLog.find('LogDetails')
-								for element in logdetails_element:
-									detail_dict = {
-										'DetailTime': element.find('DetailTime').text.strip('"'),
-										'IncidentDetail': element.find('IncidentDetail').text.strip('"')
-									}
-									LogDetails[element.tag].append(detail_dict)
-
-								pickedLogDetails = pickle.dumps(LogDetails)
-
-								if incident.LogDetails != pickedLogDetails:
-									incident.LogDetails = pickle.dumps(LogDetails)
-									incident.modified = datetime.utcnow()
-
-
-								incident.put()
-								incident_list.append(incident)
-
-						template_values['incidents'] = incident_list
-						template_values['count'] = len(incident_list)
-
+					template_values = process_chp_xml(chp_etree)
 			else:
 				error = "CHP server returned " + str(result.status_code) + " status."
 				logging.warning(error)
 				template_values['error'] = error
 
 		self.response.out.write(template.render("../templates/update.html", template_values))
+
+		def post(self):
+			data = self.request.body
+			local_sig = hmac.new("planet10", data, hashlib.sha1).hexdigest()
+
+			if self.request.headers.has_key('X-Signature') and local_sig == self.request.headers['X-Signature']:
+				# Yay, a match...
+				process_chp_xml(pickle.loads(chp_etree))
+			else:
+				# Boo, hiss, no match
+				self.response.set_status(401)
+
+
+def process_chp_xml(chpState):
+	incident_list = []
+
+	for chpCenter in chpState:
+		for chpDispatch in chpCenter:
+			# For some reason, sometimes the Dispatch ID is blank
+			# so skip these.
+			if chpDispatch.attrib['ID'] == "":
+				continue
+
+			for chpLog in chpDispatch:
+				incident = CHPIncident.get_by_key_name(chpLog.attrib['ID'])
+				if incident is None:
+					incident = CHPIncident(key_name = chpLog.attrib['ID'],
+						CenterID = chpCenter.attrib['ID'],
+						DispatchID = chpDispatch.attrib['ID'],
+						LogID = chpLog.attrib['ID'],
+						LogTime = datetime.strptime(chpLog.find('LogTime').text, '"%m/%d/%Y %I:%M:%S %p"').replace(tzinfo=Pacific()),
+						Location = deCopIfy(chpLog.find('Location').text.strip('"')),
+						Area = chpLog.find('Area').text.strip('"'),
+						ThomasBrothers = chpLog.find('ThomasBrothers').text.strip('"'),
+						TBXY = chpLog.find('TBXY').text.strip('"'),
+						modified = datetime.utcnow()
+						)
+
+					#
+					# Geocoding
+					#
+					if incident.TBXY != "":
+						tbxy = incident.TBXY.partition(":")
+						if incident.CenterID == "STCC":
+							incident.geolocation = db.GeoPt(
+								lat = float(tbxy[2]) * 0.00000274 +	 33.172,
+								lon = float(tbxy[0]) * 0.0000035  - 144.966
+							)
+						elif incident.CenterID == "SLCC":
+							incident.geolocation = db.GeoPt(
+								lat = float(tbxy[2]) * 0.00000275 +	 30.054,
+								lon = float(tbxy[0]) * 0.00000329 - 126.589
+							)
+						elif incident.CenterID == "FRCC":
+							incident.geolocation = db.GeoPt(
+								lat = float(tbxy[2]) * 0.00000274 +	 30.84,
+								lon = float(tbxy[0]) * 0.00000335 -  141
+							)
+
+				#
+				# LogType/LogTypeID
+				#
+				# I *think* the LogType/LogTypeID can change as
+				# the situation evolves.
+				#
+				# (ex: 'Traffic Collision - No Details' could
+				# become 'Possible Fatality')
+				#
+				logtype = chpLog.find('LogType').text.strip('"').partition(" - ")
+				if incident.LogTypeID != logtype[0]:
+					incident.LogTypeID = logtype[0]
+					incident.LogType = logtype[2]
+					incident.modified = datetime.utcnow()
+
+
+				#
+				# Special handling for the LogDetails
+				#
+				LogDetails = {
+					'details': [],
+					'units': []
+				}
+				logdetails_element = chpLog.find('LogDetails')
+				for element in logdetails_element:
+					detail_dict = {
+						'DetailTime': element.find('DetailTime').text.strip('"'),
+						'IncidentDetail': element.find('IncidentDetail').text.strip('"')
+					}
+					LogDetails[element.tag].append(detail_dict)
+
+				pickedLogDetails = pickle.dumps(LogDetails)
+
+				if incident.LogDetails != pickedLogDetails:
+					incident.LogDetails = pickle.dumps(LogDetails)
+					incident.modified = datetime.utcnow()
+
+
+				incident.put()
+				incident_list.append(incident)
+
+		template_values = {
+			'incidents': incident_list,
+			'count': len(incident_list)
+		}
+
+	return template_values
 
 
 # Use app cahing and regex compiling
