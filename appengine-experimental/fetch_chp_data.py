b7b41d4a2794beb1e91a00f7fd2363518f73c26e
Marc Matteo <marcm@lectroid.net>
2010-07-06 16:23:17 -0700

    Overengineer this.

--- appengine-experimental/fetch_chp_data.py
+++ appengine-experimental/fetch_chp_data.py
@@ -4,7 +4,25 @@ import hashlib
 import hmac
 import pickle
 import urllib2
+import zlib
+from optparse import OptionParser
 from xml.etree import ElementTree
+
+parser = OptionParser()
+parser.add_option("-k", action="store", type="string", dest="key",
+	help="The key to sign this data with. (required)")
+parser.add_option("-z", action="store_true", dest="gzip",
+	default=False,
+	help="If the data should be gzipped.")
+parser.add_option("-u", action="store", type="string", dest="upload_url",
+	default="http://traffic.lectroid.net/update",
+	help="URL to upload to.")
+(options, args) = parser.parse_args()
+
+# Seriously, we need a key...
+if not options.key:
+	parser.error("A key is required.")
+
 
 print "Fetching CHP data..."
 f = urllib2.urlopen("http://media.chp.ca.gov/sa_xml/sa.xml")
@@ -14,17 +32,21 @@ print "  parsing XML"
 chp_etree = ElementTree.XML(chp_data)
 
 print "  preparing upload"
-pdata = pickle.dumps(chp_etree)
+data = pickle.dumps(chp_etree)
+
+if options.gzip:
+	print "    gzipping"
+	data = zlib.compress(data)
 
 print "  signing"
-sig = hmac.new("planet10", pdata, hashlib.sha1).hexdigest()
-print "    sig: %s" % sig
+signature = hmac.new(options.key, data, hashlib.sha1).hexdigest()
+print "    sig: %s" % signature
 
 print "  uploading"
 headers = {
-	'X-Signature': sig
+	'X-Signature': signature
 }
-request = urllib2.Request('http://localhost:8080/update', pdata, headers);
+request = urllib2.Request(options.upload_url, data, headers);
 upload = urllib2.urlopen(request)
 
 print "Done."
