091d81207cd21985ece002346ff0d209aad1cc9b
Marc Matteo <marcm@lectroid.net>
2010-11-18 23:19:40 -0800

    Ok, I really wanted to run all of the CHP stuff w/in GAE but the CHP data fetch is quite slow and often errors, struggling to stay under the 10 second limit.
    So I've implemented a "helper" infrastructure that operates outside of the normal update cron function and can run under any environment (tho the enclosed script is also in Python).

---
+++ appengine/handlers/update-helper.py
@@ -0,0 +1,50 @@
+""" Provides a backup mechanism for updating the CHP data since it's sometimes
+tough for the CHP servers to respond in the GAE 10 second limit. """
+
+import hashlib
+import hmac
+import logging
+import pickle
+import time
+import zlib
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from django.utils import simplejson as json
+
+from models import CHPIncident
+from utils.process_chp_data import process_chp_xml
+# Note that the keys.py file is NOT included in public scm
+from keys import UPLOAD_KEY
+
+class UpdateHelperHandler(webapp.RequestHandler):
+	def get(self):
+		incidents = CHPIncident.all()
+		incidents.order('-updated')
+		incident = incidents.get()
+
+		if incident is not None:
+			self.response.headers["Content-Type"] = "application/json"
+			self.response.out.write(json.dumps({ 'last_update': time.mktime(incident.updated.timetuple()) }))
+
+	def post(self):
+		data = self.request.body
+		local_sig = hmac.new(UPLOAD_KEY, data, hashlib.sha1).hexdigest()
+
+		if self.request.headers.has_key('X-Signature') and local_sig == self.request.headers['X-Signature']:
+			# Yay, a match...
+			process_chp_xml(pickle.loads(zlib.decompress(data)))
+		else:
+			# Boo, hiss, no match
+			self.response.set_status(401)
+
+
+application = webapp.WSGIApplication([('/update-helper', UpdateHelperHandler)],
+										 debug=True)
+
+def main():
+	util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+	main()
