ea818bc1b5e7d647dd3e9f1ccbae19ef8cc91145
Marc Matteo <marcm@lectroid.net>
2011-11-23 21:25:31 -0800

    Ugh, I guess this is needed EVERYWHERE

--- appengine/handlers/atom.py
+++ appengine/handlers/atom.py
@@ -4,6 +4,9 @@ import re
 import time
 from xml.etree import ElementTree
 
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
