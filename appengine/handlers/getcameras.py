be078fa1389e0673880515280bc00353897c2857
Marc Matteo <marcm@lectroid.net>
2011-11-23 22:14:52 -0800

    F*** it.  Pull django 1.2 until I get how it's supposed to work.

--- appengine/handlers/getcameras.py
+++ appengine/handlers/getcameras.py
@@ -1,8 +1,4 @@
-
 import datetime
-
-from google.appengine.dist import use_library
-use_library('django', '1.2')
 
 from google.appengine.api import memcache
 from google.appengine.ext import webapp
--- appengine/handlers/update-helper.py
+++ appengine/handlers/update-helper.py
@@ -8,9 +8,6 @@ import pickle
 import time
 import zlib
 
-from google.appengine.dist import use_library
-use_library('django', '1.2')
-
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from django.utils import simplejson as json
