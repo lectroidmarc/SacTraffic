d8d745d1b96e8ef29e419f89c1ecbbf24e34a0aa
Marc Matteo <marcm@lectroid.net>
2013-07-05 19:41:55 -0700

    Match the cron description to reality.

--- appengine/handlers/purge.py
+++ appengine/handlers/purge.py
@@ -7,7 +7,7 @@ an hour from the last successful CHP data update.
 import webapp2
 from datetime import datetime, timedelta
 
-from google.appengine.ext import db
+from google.appengine.ext import ndb
 
 from models import CHPData, CHPIncident
 
@@ -18,11 +18,10 @@ class PurgeHandler(webapp2.RequestHandler):
 		chp_data_last_updated = CHPData.last_updated()
 
 		if chp_data_last_updated is not None:
-			query = CHPIncident.all(keys_only=True)
-
-			query.filter('updated <', chp_data_last_updated - timedelta(hours=1))
+			query = CHPIncident.query().filter(CHPIncident.updated < chp_data_last_updated - timedelta(minutes=15))
 			count = query.count()
-			db.delete(query)
+			keys = query.fetch(keys_only=True)
+			ndb.delete_multi(keys)
 
 		self.response.write("Purged %d records." % count)
 
